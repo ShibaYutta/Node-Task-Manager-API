1535a489a2f8b157eb66b9a5c53da5b8b4f64f41
Arthur Bispo <arthurbispo94@gmail.com>
2021-11-09 14:40:34 -0300

    Day 01
    - Basic start

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const tasks = require('./routers/tasks')
+
+// middleware
+
+app.use(express.json())
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task Maneger App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening on port ${port}...`)) 
