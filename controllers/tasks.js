7cf11d691d454355b9b517193bf46ed32858a6a7
Arthur Bispo <arthurbispo94@gmail.com>
2021-11-10 18:07:27 -0300

    Day 3
    finish and connected to the frontend

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,12 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req, res) => {
-    res.send('GET all tasks')
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 
 
@@ -9,21 +14,50 @@ const createTask = async (req, res) => {
     try {
         const task = await Task.create(req.body)
         res.status(201).json({task})
-        console.log('createTask: PASSOU UE')
     } catch (error) {
         res.status(500).json({msg:error})
-        console.log('createTask: N PASSOU')
     }
+}
+const getTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params;
+        const task = await Task.findOne({_id:taskID});
+        if(!task){
+            return res.status(404).json({msg:`No task with th id: ${taskID}`})
+        }
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg:error })
+    }
+}
+const updateTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params;
 
+        const task = await Task.findOneAndUpdate({_id:taskID},req.body,{
+            new: true,
+            runValidators: true,
+        })
+        if(!task){
+            return res.status(404).json({msg: `No task with id: ${taskID}`})
+        }
+        res.status(200).json({ task })  
+    } catch(error){
+        res.status(500).json({msg: error})
+    }
 }
-const getTask = (req, res) => {
-    res.json({id:req.params.id})
-}
-const updateTask = (req, res) => {
-    res.send('update task')
-}
-const deleteTask = (req, res) => {
-    res.send('delete task')
+const deleteTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOneAndDelete({_id:taskID});
+        if(!task){
+            return res.status(404).json({msg:`No task with id: ${taskID}`})
+        }
+
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error})
+    }
 }
 
 module.exports = {
--- models/task.js
+++ models/task.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
     name: {
+        required:[true,"Must have a name"],
         type:String,
-        require:[true,"Must have a name"],
         trim:true,
         maxlength:[20,'name can not be more than 20 characters']
     },
